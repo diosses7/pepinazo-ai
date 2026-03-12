028de73a3de11544250371b87e5597daef11e13b
diosses7 <diosses7@gmail.com>
2026-03-12 15:58:14 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Pepinazo AI está funcionando");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en puerto " + PORT);
+});
