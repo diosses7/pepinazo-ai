ed7d24e26f606eb775872cf3d936418441158af2
diosses7 <diosses7@gmail.com>
2026-03-12 17:08:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 import express from "express";
+import OpenAI from "openai";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Pepinazo AI funcionando 🚀");
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
-const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send("Pepinazo AI funcionando");
+});
 
-app.listen(PORT, () => {
-  console.log("Servidor corriendo en puerto " + PORT);
+app.listen(3000, () => {
+  console.log("Servidor corriendo");
 });
