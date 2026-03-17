87318712151a70bf26ff3c1c768230c78e876e71
diosses7 <diosses7@gmail.com>
2026-03-17 13:42:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("Pepinazo AI esta vivo");
+  res.send("Pepinazo AI está vivo");
 });
 
 app.listen(PORT, () => {
-  console.log("Servidor corriendo en puerto " + PORT);
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
